5cbbe80d18b3bc32cb6843f2979788aad3e8dc2d
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-04-06 18:40:40 -0300

    feat(Navbar): Maquetado de Navbar (#3)
    - Se creo la carpeta navbar
    - Se creo el archivo Navbar.js
    - Se creo el archivo navbar.css
    - Se creo un arreglo de objetos que contiene las key, texto y url de los enlaces correspondientes al navbar
    - Se modificaron las clases de bootstrap .nav-link y .nav-link:hover
    - Se agrego el componente Navbar a App.js para poder visualizarlo.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import Alerta from "./assets/componentes/Alerta/Alerta";
+import Alerta from "./assets/componentes/alerta/Alerta";
+import Navbar from "./assets/componentes/navbar/Navbar";
+
+
+const paginas = [
+  {
+    id: 0,
+    nombre: "Inicio",
+    url: "/inicio"
+  },
+  {
+    id: 1,
+    nombre: "Productos",
+    url: "/productos"
+  },
+  {
+    id: 2,
+    nombre: "Contacto",
+    url: "/contacto"
+  }
+];
+
 
 function App() {
   return (
-    <Alerta texto="Envío Gratis en todas tus compras superiores a 5000$"/>
+    <>
+      <Alerta texto="Envío Gratis en todas tus compras superiores a 5000$" />
+      <header>
+        <Navbar contenido={paginas} />
+      </header>
+    </>
   );
 }
 
