50f99a71bf6392b30a267086c39d46141f3607df
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-04-06 18:36:36 -0300

    feat(Navbar): Maquetado de Navbar
    - Se creo la carpeta navbar
    - Se creo el archivo Navbar.js
    - Se creo el archivo navbar.css
    - Se creo un arreglo de objetos que contiene las key, texto y url de los enlaces correspondientes al navbar
    - Se modificaron las clases de bootstrap .nav-link y .nav-link:hover
    - Se agrego el componente Navbar a App.js para poder visualizarlo.

---
+++ src/assets/componentes/navbar/Navbar.js
@@ -0,0 +1,24 @@
+import "./navbar.css";
+
+function Navbar({ contenido }) {
+    console.log(contenido)
+
+    const listaEnlaces = contenido.map(enlace => {
+        return (
+            <li className="nav-item" key={enlace.id}>
+                <a className="nav-link" href={enlace.url}>{enlace.nombre}</a>
+            </li>
+        )
+    });
+
+    return (
+        <nav>
+            <ul className="nav">
+                {listaEnlaces}
+            </ul>
+        </nav>
+    )
+
+}
+
+export default Navbar;
