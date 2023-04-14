cc5b621504fef7fb73dc1b46e4fca25230e11572
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-04-14 15:57:33 -0300

    Feature(Partner):Se completo el componente Partner
    - Se creo la carpeta partner
    - Se creo el componente Partner
    - Se agrego la imagen utilizada para el fondo del componente

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Beneficios from "./assets/componentes/beneficios/Beneficios";
 import Home from "./assets/componentes/home/Home";
 import Mision from "./assets/componentes/mision/Mision";
 import Navbar from "./assets/componentes/navbar/Navbar";
+import Partner from "./assets/componentes/partner/Partner";
 
 
 const paginas = [
@@ -34,7 +35,8 @@ function App() {
       <main className="container">
         <Home titulo="Temporada Invierno" parrafo="Encontrá las mejores prendas seleccionadas por nuestros expertos para la siguiente temporada" />
         <Beneficios />
-        <Mision/>
+        <Mision />
+        <Partner titulo="Nuestro socio benéfico" img="https://caritas.org.ar/wp-content/uploads/2021/05/logo-caritas.svg" texto="Donamos el 5% de nuestras ganancias a nuestro socio benéfico"/>
       </main>
     </>
   );
