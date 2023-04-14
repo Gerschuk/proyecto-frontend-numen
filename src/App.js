1d6847ed5f93f90c257ea17fda24992d47dd9629
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-04-14 18:06:46 -0300

    Carrusel (#21)
    * Feat(Navbar): Actualización de componentes
    
    - Se agregó un collapse
    - Se agregó el logo
    - Se agregó un borde inferior
    - Faltan estilos para desktop
    
    * Refactor(Boton): Se mejoró la interfaz (#6)
    
    - Se separaron y achicaron los botones
    - Se acomodó el badge del carro de compras
    
    * Refactor(Navbar): Se corrigió bug en button toggle
    
    * Refactor(navbar): Se modificó tamaño del Toggler
    
    -Se creo la regla .navbar-toggler-icon asignando un tamaño de .8rem al botón que muestra el menú, ya que era más grande que los botones.
    
    * Refactor(Navbar): Height de navbar -> config.css
    
    - Se paso la altura del Navbar de navbar.css -> config.css para poder acceder facilmente desde las variables css y calcular la altura del componente home
    
    * Feat(Home): Maquetando Home
    
    - Se guardo la imagen del home en assets/imagenes
    - Se creo la carpeta home
    - Se creo el archivo Home.js
    - Se creo el archivo home.css
    - Se agrego el H1 mediante props
    
    - Resta continuar maquetando el componente.
    
    * Feature(Home): Se finalizó el Home - mobile
    
    - Se agrego .btn:focus al componente Boton
    - Se finalizó la maquetación mobile de Home
    - Se agregó en config.css:
        . --h1: para controlar el tamaño de los títulos H1
    
    * Se agrego row
    
    * Feature(Carrusel): Se finalizó el componentes
    
    - Se realizó el componente Carrusel para mobile
    - Resta darle estilos para desktop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Alerta from "./assets/componentes/alerta/Alerta";
 import Beneficios from "./assets/componentes/beneficios/Beneficios";
+import Carrusel from "./assets/componentes/carrusel/Carrusel";
 import Home from "./assets/componentes/home/Home";
 import Mision from "./assets/componentes/mision/Mision";
 import Navbar from "./assets/componentes/navbar/Navbar";
@@ -24,7 +25,6 @@ const paginas = [
   }
 ];
 
-
 function App() {
   return (
     <>
@@ -36,7 +36,8 @@ function App() {
         <Home titulo="Temporada Invierno" parrafo="Encontrá las mejores prendas seleccionadas por nuestros expertos para la siguiente temporada" />
         <Beneficios />
         <Mision />
-        <Partner titulo="Nuestro socio benéfico" img="https://caritas.org.ar/wp-content/uploads/2021/05/logo-caritas.svg" texto="Donamos el 5% de nuestras ganancias a nuestro socio benéfico"/>
+        <Partner titulo="Nuestro socio benéfico" img="https://caritas.org.ar/wp-content/uploads/2021/05/logo-caritas.svg" texto="Donamos el 5% de nuestras ganancias a nuestro socio benéfico" />
+        <Carrusel/>
       </main>
     </>
   );
