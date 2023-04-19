25199718fdb2349f627fcf4d13b270a741931577
mauro <mauro.garlati@gmail.com>
2023-04-19 17:27:38 -0300

    agregue las cards

--- src/App.js
+++ src/App.js
@@ -1,79 +1,5 @@
-import Alerta from "./assets/componentes/alerta/Alerta";
-import Navbar from "./assets/componentes/navbar/Navbar";
 import React from 'react';
 import Card from "./assets/componentes/tarjetas/Card";
-
-
-
-
-
-
-
-const paginas = [
-  {
-    id: 0,
-    nombre: "Inicio",
-    url: "/inicio"
-  },
-  {
-    id: 1,
-    nombre: "Productos",
-    url: "/productos"
-  },
-  {
-    id: 2,
-    nombre: "Contacto",
-    url: "/contacto"
-  }
-];
-
-
-function App() {
-  return (
-    <>
-      <Alerta texto="Envío Gratis en todas tus compras superiores a 5000$" />
-      <header>
-        <Navbar contenido={paginas} />
-      </header>
-      <div>
-      {<h3 className="UltOfe">Ultimas Ofertas</h3>}
-      <Card
-        title="Riñonera Cuero"
-        image="https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_7633-prueba1-87e03894c9004c2b9916714768191725-320-0.webp"
-        description="Esta es una tarjeta de ejemplo"
-        precioOrigin="$3200"
-        precioDesc="$2800"
-      />
-      <Card
-        title="Gorra Negra"
-        image="https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_305311-5fc893c55d82734e9216753512387524-320-0.webp"
-        description="Esta es una tarjeta de ejemplo"
-        precioOrigin="$5000"
-        precioDesc="$4700"
-      />
-      <Card
-        title="Ropa Interior Negra"
-        image="https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_3731-311-30466bbcb580ea063316730367422774-320-0.webp"
-        description="Esta es una tarjeta de ejemplo"
-        precioOrigin="$3500"
-        precioDesc="$3000"
-      />
-      <Card
-        title="Dupla Neceser"
-        image="https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_46661-47f5345b4eb8d0f45516472717252886-320-0.webp"
-        description="Esta es una tarjeta de ejemplo"
-        precioOrigin="$4000"
-        precioDesc="$3500"
-      />
-    </div>
-    </>
-    
-  );
-
-
-}
-
-export default App;
 import Alerta from "./assets/componentes/alerta/Alerta";
 import Beneficios from "./assets/componentes/beneficios/Beneficios";
 import Carrusel from "./assets/componentes/carrusel/Carrusel";
@@ -81,6 +7,16 @@ import Home from "./assets/componentes/home/Home";
 import Navbar from "./assets/componentes/navbar/Navbar";
 import Partner from "./assets/componentes/partner/Partner";
 import BloqueLateral from "./assets/componentes/bloque-lateral/BloqueLateral";
+
+
+
+
+
+
+
+
+
+
 
 
 const paginas = [
@@ -134,8 +70,39 @@ function App() {
       <main className="container">
 
         <Home titulo="Temporada Invierno" parrafo="Encontrá las mejores prendas seleccionadas por nuestros expertos para la siguiente temporada" />
-        <Beneficios />
-
+       
+        <div>
+      {<h3 className="UltOfe">Ultimas Ofertas</h3>}
+      <Card
+        title="Riñonera Cuero"
+        image="https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_7633-prueba1-87e03894c9004c2b9916714768191725-320-0.webp"
+        description="Esta es una tarjeta de ejemplo"
+        precioOrigin="$3200"
+        precioDesc="$2800"
+      />
+      <Card
+        title="Gorra Negra"
+        image="https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_305311-5fc893c55d82734e9216753512387524-320-0.webp"
+        description="Esta es una tarjeta de ejemplo"
+        precioOrigin="$5000"
+        precioDesc="$4700"
+      />
+      <Card
+        title="Ropa Interior Negra"
+        image="https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_3731-311-30466bbcb580ea063316730367422774-320-0.webp"
+        description="Esta es una tarjeta de ejemplo"
+        precioOrigin="$3500"
+        precioDesc="$3000"
+      />
+      <Card
+        title="Dupla Neceser"
+        image="https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_46661-47f5345b4eb8d0f45516472717252886-320-0.webp"
+        description="Esta es una tarjeta de ejemplo"
+        precioOrigin="$4000"
+        precioDesc="$3500"
+      />
+    </div>
+    <Beneficios />
         <BloqueLateral props={bloqueLateral1} />
 
         <Partner titulo="Nuestro socio benéfico" img="https://caritas.org.ar/wp-content/uploads/2021/05/logo-caritas.svg" texto="Donamos el 5% de nuestras ganancias a nuestro socio benéfico" />
