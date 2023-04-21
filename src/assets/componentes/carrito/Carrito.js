af8387a05be40724ac379f44f248c5c3030b08e4
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-04-20 21:28:50 -0300

    Feature(Scroll): Se agrego Scroll

--- src/assets/componentes/carrito/Carrito.js
+++ src/assets/componentes/carrito/Carrito.js
@@ -2,7 +2,10 @@ import "./carrito.css";
 
 function Carrito() {
     return (
-        <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div className="offcanvas offcanvas-start"
+            tabindex="-1"
+            id="offcanvasExample" aria-labelledby="offcanvasExampleLabel"
+            data-bs-scroll="true">
             <div className="offcanvas-header">
                 <h3 className="offcanvas-title" id="offcanvasExampleLabel">Mi carrito</h3>
                 <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -11,7 +14,7 @@ function Carrito() {
                 <div>
                     Acá van los items del carrito
                 </div>
-                
+
             </div>
         </div>
     )
