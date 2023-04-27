2acf3b4eafb8a4e03f7fa2f53bbfe6c8d3195f26
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-04-27 18:33:58 -0300

    Bug(Resolución): al apretar boton menu

--- src/assets/componentes/navbar/Navbar.js
+++ src/assets/componentes/navbar/Navbar.js
@@ -18,8 +18,7 @@ function Navbar({ contenido }) {
                     <img className="img-fluid" src={logo} alt="Fashion Trends logo"></img>
                 </a>
 
-                
-                <div className="d-flex flex-row-reverse order-md-2">
+               <div className="d-flex flex-row-reverse  order-xl-2">
                     <button className="navbar-toggler ms-4" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
