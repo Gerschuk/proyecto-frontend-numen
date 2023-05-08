0b59ef1d37068cdae0fb398ecdc447dc77e06592
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-05-08 17:54:33 -0300

    Refactor(Home): Se obtiene el contenido por API
    - Se refactorizó la llamada al componente en App.js
    - Se refactorizó el componente Home

--- src/assets/componentes/home/Home.js
+++ src/assets/componentes/home/Home.js
@@ -2,13 +2,17 @@ import Accion from "../accion/Accion";
 import "./home.css";
 
 function Home(contenido) {
+
+    const data = contenido?.data?.find(item => item.componente === "home");
+
+    console.log(data)
     return (
 
         <div className="home row">
             <div className="contenido d-flex flex-column justify-content-end align-items-center text-center">
-                <h1 className="titulo">{contenido.titulo}</h1>
-                <p>{contenido.parrafo}</p>
-                <Accion tipo="boton" texto={contenido.textoBoton} />
+                <h1 className="titulo">{data?.titulo}</h1>
+                <p>{data?.texto}</p>
+                <Accion tipo="boton" texto={data?.textoBoton} />
             </div>
         </div>
     )
