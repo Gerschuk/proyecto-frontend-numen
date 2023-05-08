2aba9faa7641567601145376ca6d8bcf32c8cb03
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-05-08 18:54:50 -0300

    Refactor(BloqueLateral): Datos obtenidos x API
    - Se refactorizó el componente BloqueLateral para que renderice los datos obtenidos mediante el consumo de API
    - Se comprobó que la refactorización no alterara el funcionamiento de otros componentes
    - Se eliminó un console.log del componente Home

--- src/App.js
+++ src/App.js
@@ -16,29 +16,6 @@ import axios from "axios";
 // Endpoints
 import { ConexionesApi } from "./assets/Diccionario";
 
-const bloqueLateral1 = {
-  titulo: "¿Quiénes Fabrican Tus Prendas?",
-  texto: "Nuestra misión es ayudar a crecer el ecosistema local de indumentaria en Argentina y toda Latinoamérica",
-  checks: [
-    "Conectamos con los productores locales.",
-    "Porque tiene más sentido comprar algo hecho por una persona cercana que por una empresa lejana.",
-    "Porque los productos locales hacen de nuetro rincón del mundo un lugar mejor para vivir.",
-    "Porque fomentamos el empleo local.",
-    "Porque tiene más sentido para el medio ambiente."],
-  textoBoton: "Descubrí nuestros productos",
-  imagen: "https://images.pexels.com/photos/6211446/pexels-photo-6211446.jpeg",
-}
-
-const bloqueLateral2 = {
-  direccion: "izquierda",
-  titulo: "Sorprende a alguien Trabajando desde casa",
-  texto: `¿Tienes un colega o familiar que trabaja duro en casa? Nuestra caja   de prendas "Trabajar desde casa" es la forma perfecta de hacerlos sonreír, con entrega directa en su domicilio.
-        Si quieres comprar para un grupo grande de personas, ponte en contacto con nosotros y hablaremos de paquetes a medida y mensajes personalizados.`,
-  checks: null,
-  textoBoton: "Comprar Ahora",
-  imagen: "https://images.pexels.com/photos/6124349/pexels-photo-6124349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-}
-
 function App() {
 
   // Obtiene el contenido estático de la página
@@ -80,14 +57,16 @@ function App() {
         <Home data={contenido} />
         
         <Beneficios data={contenido} />
-        {/*
-        <BloqueLateral props={bloqueLateral1} />
+       
+        <BloqueLateral data={contenido} bloque="bloque1"/>
 
+        {/*
         <Partner titulo="Nuestro socio benéfico" img="https://caritas.org.ar/wp-content/uploads/2021/05/logo-caritas.svg" texto="Donamos el 5% de nuestras ganancias a nuestro socio benéfico" />
 
-        <BloqueLateral props={bloqueLateral2} />
-        <Carrusel />
         */}
+        <BloqueLateral data={contenido} bloque="bloque2" />
+        <Carrusel />
+        
       </main>
       <Footer /> 
       </>
