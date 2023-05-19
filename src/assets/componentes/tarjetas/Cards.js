04968df222d75e79c303dc2f1f89cd12af44b8b4
Gabriel Nicolás Simchuk Gerasimchuk <110862203+Gerschuk@users.noreply.github.com>
2023-05-19 15:52:02 -0300

    Modal (#46)
    * Feature(DB): Se agrego campo a la DB
    
    - Se agrego el campo "descuento" a la DB, Los descuentos se encuentran en formato decimal 10% = 0.10 En caso de no poseer descuento el producto el valor de "descuento" = null
    
    * Las tarjetas cargan contenido de API

--- src/assets/componentes/tarjetas/Cards.js
+++ src/assets/componentes/tarjetas/Cards.js
@@ -1,57 +1,28 @@
 import React from 'react'
 import Card from "./Card";
 import "./Card.css";
+import "../modal/Modal";
+import Modal from '../modal/Modal';
 
-const cards = [
-  {
-    id:1,
-    title:"Riñonera Cuero",
-    image:"https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_7633-prueba1-87e03894c9004c2b9916714768191725-320-0.webp",
-    precioO:"$3200",
-    precioD:"$2800",
-  },
-  {
-    id:2,
-    title:"Gorra Negra",
-    image:"https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_305311-5fc893c55d82734e9216753512387524-320-0.webp",
-    precioO:"$5000",
-    precioD:"$4500",
-  },
-  {
-    id:3,
-    title:"Ropa Interior Negra",
-    image:"https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_3731-311-30466bbcb580ea063316730367422774-320-0.webp",
-    precioO:"$3500",
-    precioD:"$3200",
-  },
-  {
-    id:4,
-  title:"Dupla Neceser",
-  image:"https://d3ugyf2ht6aenh.cloudfront.net/stores/988/937/products/img_46661-47f5345b4eb8d0f45516472717252886-320-0.webp",
-  precioO:"$4000",
-  precioD:"$3500",
-  }
+// Muestra los productos con descuento
+function Cards(productos) {
+  // Filtra los productos que tienen descuento
+  const productoConDescuentos = productos.data.filter((producto) => producto.descuento !== null);
 
-]
-
-function Cards() {
   return (
     <section className=" row py-5 mt-5 text-center ">
       <h2 className="titulo">Ultimas <span className="color-primario">Ofertas</span></h2>
     <div className='d-flex flex-wrap justify-content-center h-100 aling-item-center '>  
-       
      {
     
-      cards.map((card, index) =>(
+      productoConDescuentos.map((producto, index) =>(
         <div className=' col-md-3' key={index}>
-
-          <Card title={card.title} imgSource={card.image} precioD={card.precioD}precioO={card.precioO}  />
+          <Card data={producto} />
           </div>
       ))
      }
         </div>
-    
-     
+
        </section>
   
    
